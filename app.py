ef0447281922ac88a926e28b1f9dd77ad82cd0d3
Casey Johnson <cs453user@cs453host.test1>
2020-03-31 18:34:23 -0400

    hello world

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 import env
 from flask import Flask, request
-from twilio.twiml.messaging_response import Message, MessagingResponse
-from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 app.config.update(
     SECRET_KEY = env.APP_SECRET_KEY,
 )
-sms = Client(env.TWILIO_API_SID, env.TWILIO_API_TOKEN)
 
-@app.route('/sms', methods=['POST'])
+@app.route('/sms', methods=['GET','POST'])
 def sms_reply():
-    number = request.form['From']
-    body = request.form['Body']
-
     resp = twiml.MessagingResponse()
-    resp.message("Hello {}!".format(number))
+    resp.message("Hello world!")
     return str(resp)
 
 if __name__ == "__main__":

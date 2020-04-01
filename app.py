19395d2eee59da960b5a3ae7693b78e201f483ef
Casey Johnson <cs453user@cs453host.test1>
2020-04-01 17:42:20 -0400

    worked on local without HTTP POST

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ GH = geocoding.GeocodingHandler(env.LOCATION_IQ_API_KEY)
 
 def parse_message(msg):
     # temporary - assume <location>\n<route>\n<direction>
-    msg = msg[::-1]
+    msg = msg.strip()[::-1]
     di, rt, loc = msg.split('\n',maxsplit=2)
-    di, rt, loc = di[::-1], rt[::-1], loc[::-1]
+    di, rt, loc = di.strip()[::-1], rt.strip()[::-1], loc.strip()[::-1]
     return loc, rt, di
 
 
-@app.route('/sms', methods=['GET','POST'])
-def sms_reply():
+@app.route('/sms', methods=['POST'])
+def sms_reply(message):
     message = request.form['Body']
     location, route, direction = parse_message(message)
 
